118e2567810d6fb25481314fcfd09f2d2c459b3b
ShyVio <140442484+ShyVio@users.noreply.github.com>
2024-11-04 13:40:02 +0100

    Addade mitt rum

---
+++ EnterYellowRoom.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Story story = new Story();
+        Commands commands = new Commands();
+
+        story.StartStory();
+
+        while (!commands.WiresCut)
+        {
+            Console.WriteLine("\nWhat would you like to do? (type 'help' for commands)");
+            string action = Console.ReadLine()!.ToLower();
+            commands.ExecuteCommand(action, story);
+        }
+
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+}
